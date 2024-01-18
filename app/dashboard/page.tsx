88e685a18f85917a9f772d449b70496b8cc4e7b4
Fig3 <58235506+Fig3@users.noreply.github.com>
2024-01-18 19:08:20 +0900

    fix: 不要なfetchCardData()を削除

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,6 @@ export default async function Page() {
 
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const cardData = await fetchCardData();
   const {
     numberOfInvoices,
     numberOfCustomers,
